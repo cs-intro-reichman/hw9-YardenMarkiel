41f42d76a74577974d79a3187b3e520e03ba470a
YardenMarkiel <yarden.markiel@runi.co.il>
2025-01-30 13:07:20 +0200

    5

--- ListIterator.java
+++ ListIterator.java
@@ -1,18 +1,23 @@
 /** Represents an iterator of a linked list. */
 public class ListIterator {
-    private Node current;
 
+    // current position in the list (cursor)
+    public Node current;
+
+    /** Constructs a list iterator, starting at the given node */
     public ListIterator(Node node) {
         current = node;
     }
 
+    /** Checks if this iterator has more nodes to process */
     public boolean hasNext() {
         return (current != null);
     }
 
+    /** Returns the current element in the list, and advances the cursor */
     public MemoryBlock next() {
-        MemoryBlock block = current.block;
+        Node currentNode = current;
         current = current.next;
-        return block;
+        return currentNode.block;
     }
 }
